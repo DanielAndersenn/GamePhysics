9f91880ffdae8643d6e6ef99070883d2918f91cf
mathi <mathi@DESKTOP-R9517QT>
2016-12-19 17:48:34 +0100

    H E L L O

--- GamePhysics/src/classimpl/CheatSheet.java
+++ GamePhysics/src/classimpl/CheatSheet.java
@@ -24,8 +24,12 @@ public class CheatSheet {
 		//VelocityAtT(0,9.82,2.43);
 		//PositionAtT(0,0,2.35,11.8);
 		//SPLAT(50,2.35);
+		//DistanceDrivenAtVelocity(0,0,2.35,190);
+		
 		
 		//standardAirDrop(new V2(0,300),9.82,new V2(kmhTOms(500),0));
+		//projectileDistance(45,65);
+		projectileAngle(20,65);
 		
 		//calcAcc(100,0,11.8,0);
 		//System.out.println(kmhTOms(89));
@@ -33,17 +37,17 @@ public class CheatSheet {
 	}
 
 	public static void forskyd(V2 punkt, V2 vektor) {
-		
+		System.out.println("## forskyd V2, V2 ##");
 		System.out.println(punkt.add(vektor));
 	}
 	
 	public static void forskyd(V3 punkt, V3 vektor) {
-		
+		System.out.println("## forskyd V3, V3 ##");
 		System.out.println(punkt.add(vektor));
 	}
 	
 	public static void rotate(V2 punkt, double vinkel, V2 centrum) {
-		
+		System.out.println("## rotate ##");
 		V2 temp = punkt.sub(centrum);
 		
 		System.out.println(temp);
@@ -51,31 +55,33 @@ public class CheatSheet {
 	}
 	
 	public static void mirrorX(V2 v2) {
-		
+		System.out.println("## mirrorX ##");
 		System.out.println(v2.mirrorX());
 	}
 	
 	public static void mirrorY(V2 v2) {
-		
+		System.out.println("## mirrorY ##");
 		System.out.println(v2.mirrorY());
 	}
 	
+	//y = a*x + b
 	public static void mirrorLine(double a, V2 punkt) {
-		
+		System.out.println("## mirrorLine ##");
 		System.out.println(punkt.mirrorLine(a));
 	}
 	
 	public static void matriceMult(M2 matrice1, M2 matrice2) {
-		
+		System.out.println("## matriceMult M2*M2 ##");
 		matrice1.mul(matrice2);
 	}
 	
 	public static void matriceMult(M2 matrice, V2 vector) {
-		
+		System.out.println("## matriceMult M2*V2 ##");
 		matrice.mul(vector);
 	}
 	
 	public static void focus(V3 E, V3 P){
+		System.out.println("## focus ##");
 		Camera cam = new Camera();
 		cam.moveTo(E);
 		cam.focus(P);
@@ -83,10 +89,12 @@ public class CheatSheet {
 	}
 	
 	public static void terminalHastighed(double dragCoefficient){
+		System.out.println("## terminalHastighed ##");
 		System.out.println("Terminal hastighed: = "+9.82/dragCoefficient);	
 	}
 	
 	public static void coordinatesInCameraPerspective(V3 Q, V3 E,V3 O){
+		System.out.println("## coordinatesInCameraPerspective ##");
 		V3 OQ = Q.sub(O);
 		V3 OE = E.sub(O);
 		System.out.println(OQ.sub(OE));
@@ -99,11 +107,12 @@ public class CheatSheet {
 		//Beregner hastighed ved tidspunktet t
 		//DONE
 	public static void VelocityAtT(double v0, double acc, double t){
-	//v0 = Start hastighed - m/s
-	//acc = Acceleration - m/s^2
-	//t = Den ønskede tid - s
-	double result = v0+acc*t;
-	System.out.println("v(t) ="
+		System.out.println("## VelocityAtT ##");
+		//v0 = Start hastighed - m/s
+		//acc = Acceleration - m/s^2
+		//t = Den ønskede tid - s
+		double result = v0+acc*t;
+		System.out.println("v(t) ="
 			+ "\nv0 + acc * t = "
 			+"\n"+v0+" + " + acc +" * "+ t +" ="
 					+ "\n" + result+" m/s");
@@ -112,6 +121,7 @@ public class CheatSheet {
 		//Beregner position ved tidspunktet t
 		//DONE
 	public static void PositionAtT(double v0, double x0, double acc, double t){
+		System.out.println("## PositionAtT ##");
 		//v0 = start hastighed - m/s
 		//x0 = start position - m
 		//Acc = Acceleration - m/s^2
@@ -127,6 +137,7 @@ public class CheatSheet {
 		//Hvornår rammer man 0
 		//DONE
 	public static void SPLAT(double x0, double acc){
+		System.out.println("## SPLAT ##");
 		//x0 = start position - m
 		//Acc = Acceleration - m/s^2
 		double result = Math.sqrt((2*x0)/acc);
@@ -138,13 +149,18 @@ public class CheatSheet {
 	}
 	
 		//Beregner distance ved en given opnået hastighed
+		//DONE
 	public static void DistanceDrivenAtVelocity(double v0, double x0, double acc, double topSpeed){
+		System.out.println("## DistanceDrivenAtVelocity ##");
 		//double v0 = start hastighed - km/t
 		//double x0 = start position - m
 		//double acc = Acceleration - m/s^2
 		//double topSpeed = Ønskede opnået hastighed
-		
-		
+		double tempTS = kmhTOms(topSpeed);
+		double timeNeeded = tempTS/acc;
+		System.out.println("topSpeed omregnet til m/s = tempTS = "+tempTS+" m/s");
+		System.out.println("Tiden det tager at ramme topfart = tempTS/acc = "+timeNeeded+" s");
+		PositionAtT(v0,x0,acc,timeNeeded);
 	}
 	
 	
@@ -156,8 +172,10 @@ public class CheatSheet {
 	//Basis change methods
 	
 		//Input Y = a*X+b
+		//Skifter base baseret på vinkel
 		//Done
 	public static void basisChangeAngle(double angle, V2 P, V2 O){
+		System.out.println("## basisChangeAngle ##");
 		double radian = (Math.PI/180)*angle;
 		System.out.print("OP = ");
 		V2 OP = P.sub(O); System.out.println(OP);
@@ -177,8 +195,10 @@ public class CheatSheet {
 	}
 	
 		//Input Y = a*X+b
+		//Skifter base baseret på linje
 		//Done
 	public static void basisChangeLine(double a, double b, V2 P, V2 O){
+		System.out.println("## basisChangeLine ##");
 		System.out.print("OP = ");
 		V2 OP = P.sub(O); 
 		
@@ -217,7 +237,11 @@ public class CheatSheet {
 	//****************************************************
 	//Airdrop methods
 	
+		//Udregner et standard airdrop scenarie
+		//Fungerer kun ved vandret drop
+		//#TODO
 	public static void standardAirDrop(V2 r0, double acc, V2 v0){
+		System.out.println("## standardAirDrop ##");
 		//r0 = start position (x,y) - m
 		//acc = Accelerationen - m/s^2
 		//v0 = velocity / hastighed - m/s
@@ -231,6 +255,40 @@ public class CheatSheet {
 				+ Math.sqrt(2*r0.y/acc)+" s");
 		
 	}
+	
+		//Udregner hvor langt et projektil rejser
+		//DONE
+	public static void projectileDistance(double angle, double velocity){
+		System.out.println("## projectileDistance ##");
+		//angle = udgangsvinkel - grader
+		//velocity = udgangshastighed - km/t
+		double radian = angle*(Math.PI/180);
+		double speed = kmhTOms(velocity);
+		V2 V = new V2(Math.cos(radian)*speed, Math.sin(radian)*speed); 
+		System.out.println("y(t) = 0 =>"
+				+ "\nt=0 V t = (2*V0y) / g" );
+		System.out.println("Position: x(t)=V0x*t =>\nV0x * (2*V0y) / g =>");
+		System.out.println(V.x + " * " + "(2 * "+V.y +" ) / 9.82 = ");
+		double result = V.x * ((2 * V.y)/9.82);
+		System.out.println("Distance = "+result);
+	}
+	
+		//Udregner hvad udgangsvinklen er baseret på distancen
+	public static void projectileAngle(double distance ,double velocity){
+		double speed = kmhTOms(velocity);
+		
+		System.out.println("## projectileAngle ##");
+		System.out.println("x ="
+				+ "\ncos(epsilon) * velocity * ((2 * sin(epsilon) * velocity)/ g) = distance =>");
+		System.out.println("sin(2*epsilon) = (distance * g) / velocity ^2 = ");
+		System.out.println("sin(2*epsilon) = ("+distance+" * 9.82) / "+speed+"^2 =");
+		double result = (distance*9.82)/Math.pow(speed, 2);
+		System.out.println("Radianer = "+ result);
+		double radianerResult = result*(180/Math.PI);
+		System.out.println("2*epsilon = "+radianerResult);
+		System.out.println("epsilon = "+radianerResult/2);
+	}
+	
 	//****************************************************
 	
 	
@@ -238,15 +296,24 @@ public class CheatSheet {
 	
 	//****************************************************
 	//Help methods
+		//km/h til m/s
+		//DONE
 	public static double kmhTOms(double kmh){
+		System.out.println("## kmTOms ##");
 		return kmh*1000/(60*60);
 	}
 	
+		//m/s til km/h
+		//DONE
 	public static double msTOkmh(double ms){
+		System.out.println("## msTOkmh ##");
 		return ms*(60*60)/1000;
 	}
 	
+		//udregner Acc baseret på final og initial værdier
+		//DONE
 	public static double calcAcc(double vf, double vi, double tf, double ti){
+		System.out.println("## calcAcc ##");
 		//vf = velocity final - km/t
 		//vi = velocity intial - km/t
 		//tf = time final - s
